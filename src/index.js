f1e197e2d02f43eeb8d10e416e8149c2fd368339
stryker-e <emily.stryker4@gmail.com>
2023-01-04 00:01:58 -0500

    :>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! Love, Peck.');
+});
 
 app.listen(3000, () =>
     console.log('Example app listening on port 3000'),
