39437742600c4e7dd7aceeee15603e0787f5cd6a
stryker-e <emily.stryker4@gmail.com>
2023-01-05 15:57:13 -0500

    wait oh yes wait a minute mr postman

--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@ const app = express();
 
 app.use(cors());
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World! Love, Peck.');
+// });
+
 app.get('/', (req, res) => {
-    res.send('Hello World! Love, Peck.');
+    return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(process.env.PORT, () =>
